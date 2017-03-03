2b548d76caf748fe93a57e99c6eb8066f43c3aa2
KensoDev <avi@kensodev.com>
2017-03-02 23:25:51 -0800

    day 2 working version of react app for the live stream

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>This is a react app</h1>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var path = require('path');
+var polyfill = require("babel-polyfill");
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    },
+    {
+      test: /\.(scss|css)$/,
+      loader: "style!css!sass?includePaths[]=" + path.resolve(__dirname, "./node_modules/compass-mixins/lib")
+    },
+    {
+      test: /\.less$/,
+      loader: "style-loader!css-loader!less-loader"
+    },
+    {
+      test: /\.(eot|svg|ttf|woff2|woff|png|jpe?g|gif)$/,
+      loader: 'file-loader'
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api*': 'http://localhost:3000',
+    }
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  },
+  plugins: [
+  ]
+};
